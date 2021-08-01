6043087fe0c405111a2a2b22ff6678ddcf63a4ea
Pijush Barik <pijush.barik8@gmail.com>
2021-08-01 21:34:26 +0530

    added Carousel component

---
+++ src/components/ui/Carousel/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Slick, { Settings } from "react-slick";
+import Helmet from "react-helmet";
+
+type CarouselProps = {
+  slickSettings: Settings;
+};
+
+const Carousel: React.FC<CarouselProps> = props => {
+  return (
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
+        />
+      </Helmet>
+      <Slick {...props.slickSettings}>{props.children}</Slick>
+    </>
+  );
+};
+
+export default Carousel;
